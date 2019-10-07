90ffe60c6e7ba51e14c3226ae7e0a7394fa4d299
Kavan Samra <kssamra@ucsc.edu>
2019-10-06 18:40:31 -0700

    All launches displayed with their mission title and launch date

--- client/src/components/Launches/Launches.js
+++ client/src/components/Launches/Launches.js
@@ -1,6 +1,7 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
+import LaunchItem from './LaunchItem/LaunchItem';
 
 const LAUNCHES_QUERY = gql`
   query LaunchesQuery{
@@ -16,19 +17,25 @@ const LAUNCHES_QUERY = gql`
 class Launches extends Component {
   render(){
     return (
-      <div>
+      <Fragment>
         <h1 className="display-4 my-3" > Launches </h1>
         <Query query={LAUNCHES_QUERY}>
           {
             ({ loading, error, data }) => {
               if (loading) return <h4>Loading...</h4>
               if (error) console.log(error)
-              console.log(data);
-              return <h1> Test </h1>
+              
+              return <Fragment>
+                {
+                  data.launches.map(launch => {
+                    return <LaunchItem key={launch.flight_number} launch={launch}/>
+                  })
+                }
+              </Fragment>
             }
           }
         </Query>
-      </div>
+      </Fragment>
     )
   }
 };
